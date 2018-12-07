b26f78394f7e002929a6bf7afb8131e7d79dd3f5
Evan Oliver <olleit@gmail.com>
2018-12-06 19:41:27 -0600

    Added top navigation component and made responsive adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './topnav/topnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/topnav/topnav.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-topnav',
+  templateUrl: './topnav.component.html',
+  styleUrls: ['./topnav.component.sass']
+})
+export class TopnavComponent implements OnInit {
+  @Input() title: String;
+  isCollapsed = true;
+  toggleCollapsed(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
