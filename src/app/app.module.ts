e7165002a95d44289d7c4c5f63b554e3ba763692
Evan Oliver <olleit@gmail.com>
2018-12-07 00:49:39 -0600

    Made various style customizations in theme, added components for demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { TopnavComponent } from './topnav/topnav.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopnavComponent
+    TopnavComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pagination/pagination.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.sass']
+})
+export class PaginationComponent implements OnInit {
+  page = 1;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
